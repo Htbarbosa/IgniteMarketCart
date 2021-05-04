c53aaad87348fd62e91ba9d8986182f77e721526
Henrique Teles Barbosa <henrique_telesbarbosa@hotmail.com>
2021-05-04 19:05:23 -0300

    hookUseCart

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -5,8 +5,8 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
+import { useCart } from '../../hooks/useCart';
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
 interface Product {
@@ -18,17 +18,17 @@ interface Product {
 }
 
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
+  const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const cartFormatted = cart.map(product => ({
+    // TODO
+  }))
+  const total =
+    formatPrice(
+      cart.reduce((sumTotal, product) => {
+        // TODO
+      }, 0)
+    )
 
   function handleProductIncrement(product: Product) {
     // TODO
